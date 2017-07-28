78f092990487ceca28a29fb39cf74b53954aac12
phantasy <martin@ammarnas.nu>
2017-07-28 22:43:50 +0200

    fixed bad merge

--- src/pc.c
+++ src/pc.c
@@ -361,11 +361,8 @@ void resetpchard()
         closeal();
 
         device_init();
-<<<<<<< HEAD
         midi_device_init();
         inital();
-=======
->>>>>>> refs/remotes/origin/upstream
         
         timer_reset();
         sound_reset();
